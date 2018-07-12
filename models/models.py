c1a4d03f792c82ea83137dec3352b85b5533ad67
MitchelHdz <sebastianvicky_@hotmail.com>
2018-07-12 17:34:52 -0500

    [WIP] Model relations lesson

---
+++ models/models.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
+class Course(models.Model):
+	_name = 'openacademy.course'
+
+	name = fields.Char(string="Title", required=True)
+	description = fields.Text()
+
+class Session(models.Model):
+	_name = 'openacademy.session'
+
+	name = fields.Char(required=True)
+	start_date = fields.Date()
+	duration = fields.Float(digits=(6, 2), help="Duration in days")
+	seats = fields.Integer(string="Number of seats")
+		
