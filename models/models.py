5c65f8d64260c9ae85d03b318f80a0f448feb4b2
MitchelHdz <sebastianvicky_@hotmail.com>
2018-07-13 17:56:42 -0500

    [REF] 'Onchange/Constraints lesson done'

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
@@ -20,6 +20,30 @@ class Course(models.Model):
 	description = fields.Text()
 	responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
 	session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+	
+	@api.multi
+	def copy(self, default=None):
+		default = dict(default or {})
+
+		copied_count = self.search_count(
+			[('name', '=like', u"Copy of {}%".format(self.name))])
+		if not copied_count:
+			new_name = u"Copy of {}".format(self.name)
+		else:
+			new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+		default['name'] = new_name
+		return super(Course, self).copy(default)
+
+	_sql_constraints = [
+		('name_description_check',
+		'CHECK(name != description)',
+		'The title of the course should not be the description'),
+
+		('name_unique',
+		'UNIQUE(name)',
+		'The course title must be unique'),
+	]
 
 class Session(models.Model):
 	_name = 'openacademy.session'
@@ -45,3 +69,26 @@ class Session(models.Model):
 				r.taken_seats = 0.0
 			else:
 				r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+	@api.onchange('seats', 'attendee_ids')
+	def _verify_valid_seats(self):
+		if self.seats < 0:
+			return {
+				'warning': {
+					'title': "Incorrect 'seats' value",
+					'message': "The number of available seats may not be negative",
+				},
+			}
+		if self.seats < len(self.attendee_ids):
+			return {
+				'warning': {
+					'title': "Too many attendees",
+					'message': "Increase seats or remove excess attendees",
+				},
+			}
+
+	@api.constrains('instructor_id','attendee_ids')
+	def _check_instructor_not_in_attendees(self):
+		for r in self:
+			if r.instructor_id and r.instructor_id in r.attendee_ids:
+				raise exceptions.ValidationError("A session's instructor can't be an attendee")
