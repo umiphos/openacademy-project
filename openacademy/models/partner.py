c44b2804e0d9a1f2a991fa596da145e8da429ab8
MitchelHdz <sebastianvicky_@hotmail.com>
2018-07-24 11:50:32 -0500

    [REF] Models: Indentation changed to 4 spaces instead of tabs

--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -2,7 +2,7 @@
 from odoo import fields, models
 
 class Partner(models.Model):
-	_inherit = 'res.partner'
+    _inherit = 'res.partner'
 
-	instructor = fields.Boolean("Instructor", default=False)
-	session_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
+    instructor = fields.Boolean("Instructor", default=False)
+    session_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
